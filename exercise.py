f5864af119833029b2d3219e52f176ce86a74941
Ryan Farrell <fyanrarrell@gmail.com>
2022-06-29 19:39:33 -0700

    trimmed SMILE

--- exercise.py
+++ exercise.py
@@ -2,4 +2,9 @@ import csv
 file = open("chemid-sample.csv")
 filecontent = csv.reader(file)
 for row in filecontent:
-    print(row)
+    # print(row[5])
+
+    a_string=row[5]
+    for character in '([=#)]':
+        a_string = a_string.replace(character, '')
+    print(a_string)
